ac395fc50bc2a28472c6593aa44b227aec702732
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-09-01 21:28:29 +0530

    [Devesh]: Added ability to detect happy or sad mood

--- src/main/java/com/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/moodanalyzer/MoodAnalyzer.java
@@ -1,5 +1,15 @@
 package com.moodanalyzer;
 
 public class MoodAnalyzer {
-
+	
+	public String analyzeMood(String message) {
+		
+		if (message.contains("happy")) return "Happy";
+		
+		//TC 1.1 taken care below
+		else if (message.contains("sad")) return "Sad";
+		
+		//TC 1.2 taken care below
+		else return "Happy";
+	}
 }
--- src/test/java/com/moodanalyzertest/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzertest/MoodAnalyzerTest.java
@@ -1,5 +1,31 @@
 package com.moodanalyzertest;
+import com.moodanalyzer.MoodAnalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class MoodAnalyzerTest {
-
+	
+	@Test
+	public void givenMood_WhenHavingHappy_ShouldReturnHappy() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String result = moodAnalyzer.analyzeMood("I am in happy mood.");
+		Assert.assertEquals("Happy", result);
+	}
+	
+	//TC 1.1
+	@Test
+	public void givenMood_WhenHavingSad_ShouldReturnSad() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String result = moodAnalyzer.analyzeMood("I am in sad mood.");
+		Assert.assertEquals("Sad", result);
+	}
+	
+	//TC 1.2
+	@Test
+	public void givenMood_WhenHavingAny_ShouldReturnHappy() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String result = moodAnalyzer.analyzeMood("I am in any mood.");
+		Assert.assertEquals("Happy", result);
+	}
 }
